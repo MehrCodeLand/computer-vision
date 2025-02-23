386b403c7c3df2301883a3d48b40e3b3eb5f31a6
Mehrshad <lastenigmas@gmail.com>
2025-02-23 13:22:25 +0330

    update

--- detect_object/sift.py
+++ detect_object/sift.py
@@ -1,20 +1,19 @@
 import cv2
 import numpy as np
 
-
 img = cv2.imread('images/part.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-sift = cv2.SIFT()
+# Use SIFT_create() to create a SIFT detector
+sift = cv2.SIFT_create()
 
-# keypoint
+# Detect keypoints
 keypoints = sift.detect(gray, None)
-print( 'key point : ', len(keypoints))
+print('key point : ', len(keypoints))
 
+# Draw keypoints
+img_with_keypoints = cv2.drawKeypoints(img, keypoints, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-img = cv2.drawKeypoints(img , keypoints , flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-
-cv2.imshow('keypoints', img)
+cv2.imshow('keypoints', img_with_keypoints)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
